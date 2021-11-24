5f358d1703a16f497aee9f1c89b3ad19a60f9e31
Intro-Dev <72041837+Intro-Dev@users.noreply.github.com>
2021-11-24 18:41:29 -0500

    Complete overhaul of option system.

---
+++ src/main/java/com/intro/common/config/options/legacy/LegacyOption.java
@@ -0,0 +1,20 @@
+package com.intro.common.config.options.legacy;
+
+public abstract class LegacyOption {
+
+    public final String identifier;
+
+    public final String type;
+
+    public LegacyOption(String identifier, String type) {
+        this.identifier = identifier;
+        this.type = type;
+    }
+
+    public abstract LegacyOption get();
+
+    public abstract void put();
+
+
+
+}
