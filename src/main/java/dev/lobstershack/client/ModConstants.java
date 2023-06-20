7ee90fcccc2a3cc0abc58a85a7b4a046b3f036a7
Intro-Dev <72041837+Intro-Dev@users.noreply.github.com>
2023-06-20 17:29:05 -0400

    Bump to 1.20.1

--- src/main/java/dev/lobstershack/client/ModConstants.java
+++ src/main/java/dev/lobstershack/client/ModConstants.java
@@ -3,6 +3,6 @@ package dev.lobstershack.client;
 public class ModConstants {
 
     public static final String UPDATE_STRING = "1.4.1";
-    public static final String MINECRAFT_VERSION_STRING = "1.20";
+    public static final String MINECRAFT_VERSION_STRING = "1.20.1";
     public static final String FULL_VERSION_STRING = UPDATE_STRING + "-" + MINECRAFT_VERSION_STRING;
 }
